22e4f677152a6458d3745fbbf4984b79d2d5b04c
githubscientist <sathish.datascientist@gmail.com>
2025-06-24 19:05:19 +0530

    class components in react

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 //     return <h1>Hello, World!</h1>
 // }
 
+
 // functional component using function expression
 // const App = function () {
 //     return <h1>Hello, World!</h1>
@@ -11,8 +12,30 @@
 // functional component using arrow function
 // const App = () => <h1>Hello, World!</h1>;
 
-const App = () => {
-    return <h1>Hello, World!</h1>;
+// const App = () => {
+//     return <h1>Hello, World!</h1>;
+// }
+
+// class component in React
+
+/*
+    React -> namespace from 'react' library or module or package
+    namespace -> a way to group related code together
+    React.Component -> base class for creating class components
+
+    the class App is extending React.Component, which means it inherits all the properties and methods of React.Component.
+
+    render() -> method that returns the JSX to be rendered
+    This render method is called to override the default render method of React.Component.
+
+    Method Overriding -> a way to change the behavior of a method in a subclass to execute it over the method in the parent class.
+*/
+
+import React from "react";
+class App extends React.Component {
+    render() {
+        return <h1>Hello, World!</h1>;
+    }
 }
 
 export default App;
