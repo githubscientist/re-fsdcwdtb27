740494c71282b931eb8b56b4d3476f0261d58830
githubscientist <sathish.datascientist@gmail.com>
2025-06-24 19:56:18 +0530

    using variables in a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,23 @@
-// functional component in React
-// function App() {
-//     return <h1>Hello, World!</h1>
-// }
-
-
-// functional component using function expression
-// const App = function () {
-//     return <h1>Hello, World!</h1>
-// }
-
-// functional component using arrow function
-// const App = () => <h1>Hello, World!</h1>;
-
-// functional component using arrow function
-// multiple components using functional components
-// data passing in functional components
-const Hello = (props) => {
-    return <h1>Hello, {props.name}!</h1>;
-}
-
+// rendering -> means the component is inserted into the DOM
+// re-rendering -> means the component is updated in the DOM
 const App = () => {
-    return <Hello
-        name='Denis'
-    />;
+    // ordinary variable
+    // updating this variable will not cause the component to re-render
+    // so the UI will not update
+    // this is not reactive
+    let count = 0;
+
+    const handleIncrease = () => {
+        count = count + 1;
+        console.log('Count:', count);
+    }
+
+    return (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={handleIncrease}>Increase</button>
+        </div>
+    )
 }
 
 export default App;
-
-// class component in React
-
-/*
-    React -> namespace from 'react' library or module or package
-    namespace -> a way to group related code together
-    React.Component -> base class for creating class components
-
-    the class App is extending React.Component, which means it inherits all the properties and methods of React.Component.
-
-    render() -> method that returns the JSX to be rendered
-    This render method is called to override the default render method of React.Component.
-
-    Method Overriding -> a way to change the behavior of a method in a subclass to execute it over the method in the parent class.
-*/
-
-// multiple components using class components
-// passing data in class components
-// import React from "react";
-
-// class Hello extends React.Component {
-
-//     constructor(props) {
-//         // calling the parent class constructor
-//         super(props);
-//     }
-
-//     render() {
-//         return <h1>Hello, {this.props.name}!</h1>;
-//     }
-// }
-
-// class App extends React.Component {
-//     render() {
-//         return <Hello
-//             name='Denis'
-//         />;
-//     }
-// }
-
-// export default App;
