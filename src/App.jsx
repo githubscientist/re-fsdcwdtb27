07c0450d67d207d847f8549793c54e9ebd7f489f
githubscientist <sathish.datascientist@gmail.com>
2025-06-25 18:45:34 +0530

    useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
+/*
+    useEffect
+    - useEffect is a hook that allows you to perform side effects in function components.
+*/
+
 import { useEffect, useState } from "react";
 
 const App = () => {
 
     const [counter, setCounter] = useState(0);
+    const [count, setCount] = useState(0);
 
-    // this effect runs only once when the component mounts
+    // this effect runs when the component mounts and whenever any state or prop changes
+    // useEffect(() => {
+    //     console.log("Counter value:", counter);
+    // });
+
+    // this effect runs only when the component mounts
+    // useEffect(() => {
+    //     console.log("Counter value:", counter);
+    // }, []);
+
+    // this effect runs when the component mounts and whenever the counter state changes.
     useEffect(() => {
         console.log("Counter value:", counter);
-    }, []);
+    }, [counter]);
+
+    useEffect(() => {
+        console.log("Count value:", count);
+    }, [count]);
+
+    const handleIncrease = () => {
+        setCounter(counter + 1);
+        // setCount(count + 1);
+    }
 
     return (
         <div>
-            <h1>Counter: {counter}</h1>
-            <button onClick={() => setCounter(counter + 1)}>Increase</button>
+            <h1>Counter: {count}</h1>
+            <button onClick={handleIncrease}>Increase</button>
         </div>
     )
 }
