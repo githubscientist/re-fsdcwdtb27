ceb8ce60413cb7771f062a1b6c56e7e08564d85f
githubscientist <sathish.datascientist@gmail.com>
2025-06-25 20:28:43 +0530

    rendering api data into jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+/*
+    key prop: it is used to uniquely identify elements in a list.
+*/
+
 import { useEffect, useState } from "react";
 
 const App = () => {
 
     const [todos, setTodos] = useState([]);
 
+    // runs only once
     useEffect(() => {
         fetch(`https://685ac3af9f6ef9611157b188.mockapi.io/todos`)
             .then(response => response.json())
@@ -13,7 +18,12 @@ const App = () => {
     console.log(todos);
 
     return (
-        <div>App</div>
+        <div>
+            <h1>Todos</h1>
+            <ul>
+                {todos.map(todo => <li key={todo.id}>{todo.content}</li>)}
+            </ul>
+        </div>
     )
 }
 
