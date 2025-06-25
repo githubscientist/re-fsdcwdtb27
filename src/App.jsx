2d7edb1ee4d67726ebb6158c162618f40552734e
githubscientist <sathish.datascientist@gmail.com>
2025-06-25 20:47:11 +0530

    controlled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+/*
+    uncontrolled component:
+
+    - a component that does not manage its own state.
+
+    controlled component: 
+
+    - a component that manages its own state.
+*/
+
+import { useState } from "react";
+
 const App = () => {
 
-    // uncontrolled component
-    // using a form to handle registration
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const handleRegister = (event) => {
         event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value;
 
         console.log('Registering user!');
         console.log('Email:', email);
@@ -19,12 +30,16 @@ const App = () => {
                 type="email"
                 placeholder="Email..."
                 name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
             />
 
             <input
                 type="password"
                 placeholder="Password..."
                 name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
             />
 
             <button type="submit">Register</button>
