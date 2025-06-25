7a698f6ed5c190fa09db440fa8e93b6ba28e05fa
githubscientist <sathish.datascientist@gmail.com>
2025-06-25 19:03:09 +0530

    api calling using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,19 @@
-/*
-    useEffect
-    - useEffect is a hook that allows you to perform side effects in function components.
-*/
-
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const App = () => {
 
-    const [counter, setCounter] = useState(0);
-    const [count, setCount] = useState(0);
-
-    // this effect runs when the component mounts and whenever any state or prop changes
-    // useEffect(() => {
-    //     console.log("Counter value:", counter);
-    // });
-
-    // this effect runs only when the component mounts
-    // useEffect(() => {
-    //     console.log("Counter value:", counter);
-    // }, []);
-
-    // this effect runs when the component mounts and whenever the counter state changes.
-    useEffect(() => {
-        console.log("Counter value:", counter);
-    }, [counter]);
-
-    useEffect(() => {
-        console.log("Count value:", count);
-    }, [count]);
-
-    const handleIncrease = () => {
-        setCounter(counter + 1);
-        // setCount(count + 1);
+    const fetchTodos = async () => {
+        const response = await fetch(`https://685ac3af9f6ef9611157b188.mockapi.io/todos`);
+        const todos = await response.json();
+        console.log(todos);
     }
 
+    useEffect(() => {
+        fetchTodos();
+    }, []);
+
     return (
-        <div>
-            <h1>Counter: {count}</h1>
-            <button onClick={handleIncrease}>Increase</button>
-        </div>
+        <div>App</div>
     )
 }
 
