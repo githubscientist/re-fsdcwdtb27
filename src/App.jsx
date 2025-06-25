5d0d91db3f0dff3c5586c34065d3643ee58934e1
githubscientist <sathish.datascientist@gmail.com>
2025-06-25 19:17:36 +0530

    useEffect + fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
+
+    const [todos, setTodos] = useState([]);
 
     useEffect(() => {
         fetch(`https://685ac3af9f6ef9611157b188.mockapi.io/todos`)
             .then(response => response.json())
-            .then(todos => console.log(todos));
-
+            .then(data => setTodos(data));
     }, []);
+
+    console.log(todos);
 
     return (
         <div>App</div>
