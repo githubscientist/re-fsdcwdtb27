70a0e7706f783630e34435435444abf20054d330
githubscientist <sathish.datascientist@gmail.com>
2025-06-26 20:21:20 +0530

    useRef var persist across rerenders

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const App = () => {
 
     const [state, setState] = useState(0);
-    let [count, setCount] = useState(0);
+    let count = useRef(0);
 
     const handleIncrease = () => {
-        setCount(count + 1);
-        console.log("Count:", count);
+        count.current += 1;
+        console.log("Count:", count.current);
     }
 
     return (
         <div>
-            <h1>Count: {count}</h1>
+            <h1>Count: {count.current}</h1>
             <button onClick={handleIncrease}>Increase</button>
             <button onClick={() => setState(state + 1)}>Re-Render</button>
         </div>
