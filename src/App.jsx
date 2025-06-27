6908fdb26aba19fcc23f60ace713b3f759250e4d
githubscientist <sathish.datascientist@gmail.com>
2025-06-27 19:01:28 +0530

    useRef hook --mutable value --recap done

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,32 @@
     useRef Hook:
 
     - useRef is a React Hook that allows you to select a DOM element directly instead of the old way of using document.getElementById or document.querySelector.
+
+    1. To get a reference to a DOM element.
+    2. The value of the ref object is mutable and can be changed without causing a re-render.
+    3. When the component re-renders, the ref object persists the same reference or value across the renders.
 */
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const App = () => {
 
-    const header = useRef(null);
+    let [refresh, setRefresh] = useState(0);
+    let count = useRef(0);
 
     const handleChange = () => {
-        header.current.textContent = 'Hello, World!';
-        header.current.style.color = 'blue';
+        count.current += 1;
+
+        console.log(count.current);
     }
+
+    console.log("Component rendered", refresh);
 
     return (
         <div>
-            <h1 ref={header}>Hello, Welcome!</h1>
-            <button onClick={handleChange}>Change Text</button>
+            <h1>Count: {count.current}</h1>
+            <button onClick={handleChange}>Increase</button>
+            <button onClick={() => setRefresh(refresh + 1)}>Re-Render Component</button>
         </div>
     )
 }
