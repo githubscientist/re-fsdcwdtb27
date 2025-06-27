331f0de8493d350cecafa80d490ee58c0bfc77a9
githubscientist <sathish.datascientist@gmail.com>
2025-06-27 20:27:32 +0530

    useState example

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,17 @@
-/*
-    useReducer:
-
-    - useReducer is a React hook that is used to manage complex state logic in functional components.
-    - It is an alternative to useState and is particularly useful when the state logic involves multiple sub-values or when the next state depends on the previous one.
-*/
-
-import { useReducer } from "react";
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'SET_NAME':
-            return {
-                ...state,
-                name: action.payload
-            };
-        case 'SET_EMAIL':
-            return {
-                ...state,
-                email: action.payload
-            }
-        case 'SET_PASSWORD':
-            return {
-                ...state,
-                password: action.payload
-            }
-    }
-}
+import { useState } from "react";
 
 const App = () => {
 
-    const [state, dispatch] = useReducer(reducer, {
-        name: '',
-        email: '',
-        password: ''
-    });
+    const [likes, setLikes] = useState(0);
 
-    const handleRegister = (e) => {
-        e.preventDefault();
-        console.log('Registering user:', state);
+    const handleReaction = () => {
+        setLikes(likes + 1);
     }
 
     return (
         <div>
-            <h1>Register</h1>
-            <form onSubmit={handleRegister}>
-                <div>
-                    <input
-                        type="text"
-                        placeholder="Name..."
-                        value={state.name}
-                        onChange={(e) => dispatch({ type: 'SET_NAME', payload: e.target.value })}
-                    />
-                </div>
-                <br />
-                <div>
-                    <input
-                        type="email"
-                        placeholder="Email..."
-                        value={state.email}
-                        onChange={(e) => dispatch({ type: 'SET_EMAIL', payload: e.target.value })}
-                    />
-                </div>
-                <br />
-                <div>
-                    <input
-                        type="password"
-                        placeholder="Password..."
-                        value={state.password}
-                        onChange={(e) => dispatch({ type: 'SET_PASSWORD', payload: e.target.value })}
-                    />
-                </div>
-                <br />
-                <button type="submit">Submit</button>
-            </form>
+            <h1>Likes: {likes}</h1>
+            <button onClick={handleReaction}>Like</button>
         </div>
     )
 }
