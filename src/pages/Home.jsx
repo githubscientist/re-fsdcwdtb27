36612e8db08fda09fefd4f928955644ecb5c9224
githubscientist <sathish.datascientist@gmail.com>
2025-07-02 19:26:28 +0530

    setting api data to state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://685ac3af9f6ef9611157b188.mockapi.io/todos`)
+            .then(response => response.json())
+            .then(data => setTodos(data));
+    }, []);
+
+    console.log(todos);
+
     return (
         <div>
             <h1>Hello World!</h1>
