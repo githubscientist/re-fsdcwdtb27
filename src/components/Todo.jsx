6ef3fed99054bfa40bf5330d6ea5ec4b76768cc3
githubscientist <sathish.datascientist@gmail.com>
2025-07-03 18:59:20 +0530

    getting the id using useParams hook

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,11 @@
+import { useParams } from "react-router";
+
 const Todo = () => {
+
+    const params = useParams();
+
+    console.log(params);
+
     return (
         <div>
             <h1>Todo</h1>
